b9d2f56d2b71d5f66fc43ff59a48368fdfafbde9
datle-dev <dle.dev@outlook.com>
2024-03-01 22:05:59 -0500

    Set up schema for astro content collections

---
+++ src/content/config.ts
@@ -0,0 +1,24 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+// 2. Define your collection(s)
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        isDraft: z.boolean(),
+        tags: z.array(z.string()),
+        category: z.string(),
+        image: z.object({
+            src: z.string(),
+            alt: z.string(),
+        }).optional(),
+        publishDate: z.date(),
+        modifyDate: z.date().optional(),
+    })
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  'blog': blogCollection,
+};
