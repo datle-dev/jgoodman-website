8a5bee4aac149a2e2081a3946afd961693a74251
datle-dev <dle.dev@outlook.com>
2024-03-01 21:52:34 -0500

    Configure astro project for GitHub pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://datle-dev.github.io',
+    base: '/jgoodman-website/',
+});
