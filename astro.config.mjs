1a79ff5a109088ee4f05e9107be2dd8dd28a95b4
datle-dev <dle.dev@outlook.com>
2024-03-01 22:12:55 -0500

    Incorporate pagefind search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://datle-dev.github.io',
     base: '/jgoodman-website/',
+    // pagefind needs trailing backslash in base, otherwise path
+    // of js file isn't correct
+    build: {
+        format: 'file',
+    },
+    integrations: [
+        pagefind(),
+    ],
 });
