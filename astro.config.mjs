0405f35e8f8578c67ba5f2a959941783e0ce3f2d
datle-dev <dle.dev@outlook.com>
2024-03-02 13:10:34 -0500

    Add astro-mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
 import { defineConfig } from 'astro/config';
 import pagefind from "astro-pagefind";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://datle-dev.github.io',
-    base: '/jgoodman-website/',
-    // pagefind needs trailing backslash in base, otherwise path
-    // of js file isn't correct
-    build: {
-        format: 'file',
-    },
-    integrations: [
-        pagefind(),
-    ],
-    markdown: {
-        shikiConfig: {
-            theme: 'github-light',
-        },
-    },
+  site: 'https://datle-dev.github.io',
+  base: '/jgoodman-website/',
+  // pagefind needs trailing backslash in base, otherwise path
+  // of js file isn't correct
+  build: {
+    format: 'file'
+  },
+  integrations: [pagefind(), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-light'
+    }
+  }
 });
