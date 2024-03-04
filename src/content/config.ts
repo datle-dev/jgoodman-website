eb895e09fa4ac3a0584005a552312936b2588c73
datle-dev <dle.dev@outlook.com>
2024-03-04 15:50:47 -0500

    Add unique schema and collections object for 90s content

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,8 +17,25 @@ const blogCollection = defineCollection({
         modifyDate: z.date().optional(),
     })
 });
+const blogCollection90s = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        draft: z.boolean(),
+        tags: z.array(z.string()),
+        category: z.string(),
+        image: z.object({
+            src: z.string(),
+            alt: z.string(),
+        }).optional(),
+        publishDate: z.date(),
+        modifyDate: z.date().optional(),
+    })
+});
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'blog': blogCollection,
+  'blog-90s': blogCollection90s,
 };
