dcfef0cc50a6047922a757212c201d7505bb5b72
datle-dev <dle.dev@outlook.com>
2024-08-27 08:36:28 -0400

    Initial commit

---
+++ src/lib/utils.ts
@@ -0,0 +1,37 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: Date) {
+  return Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "2-digit",
+    year: "numeric"
+  }).format(date)
+}
+
+export function readingTime(html: string) {
+  const textOnly = html.replace(/<[^>]+>/g, "")
+  const wordCount = textOnly.split(/\s+/).length
+  const readingTimeMinutes = ((wordCount / 200) + 1).toFixed()
+  return `${readingTimeMinutes} min read`
+}
+
+export async function getAlbumImages(albumId: string) {
+  let images = import.meta.glob<{ default: ImageMetadata }>(
+    "/src/content/albums/**/*.{jpeg,jpg,png}"
+  );
+
+  images = Object.fromEntries(
+    Object.entries(images).filter(([key]) => key.includes(albumId))
+  );
+
+  const resolvedImages = await Promise.all(
+    Object.values(images).map((image) => image().then((mod) => mod.default))
+  );
+
+  return resolvedImages;
+}
